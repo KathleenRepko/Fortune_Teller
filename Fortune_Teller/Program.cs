59d93529c36aa354d6dd7a3637eae4c54df0c37b
KathleenRepko <kathleen.repko@gmail.com>
2017-09-29 14:20:12 -0400

    Completed user input.

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -8,6 +8,8 @@ namespace Fortune_Teller
 {
     class Program
     {
+        private static string faveColor2;
+
         static void Main(string[] args)
         {
             //Correct GitHub link is properly submitted
@@ -22,59 +24,133 @@ namespace Fortune_Teller
             //Ask the user for the number of siblings the user has
 
             Console.WriteLine("Welcome to Madame Sybil's Fortune Telling Tent. I knew you were coming.");
-            Console.WriteLine("What is your first name? Type it, and then click \"Enter.\"");
+            Console.WriteLine("\nWhat is your first name? Type it, and then click \"Enter.\"");
             string firstName = Console.ReadLine();
-            Console.WriteLine("Lovely! Thank you!");
-            Console.WriteLine("What is your last name? Type it, and then click \"Enter.\"");
+            Console.WriteLine("\nLovely! Thank you!");
+
+            Console.WriteLine("\nWhat is your last name? Type it, and then click \"Enter.\"");
             string lastName = Console.ReadLine();
-            Console.WriteLine("I new a " + lastName + " family from Poughkeepsie. Uncanny!");
-            Console.WriteLine("How many years have you been alive? Type a number, and then click \"Enter.\"");
+            Console.WriteLine("\nI new a " + lastName + " family from Poughkeepsie. Uncanny!");
+
+            Console.WriteLine("\nHow many years have you been alive? Type a number, and then click \"Enter.\"");
             int userAge = int.Parse(Console.ReadLine());
             if (userAge < 30)
             {
-                Console.WriteLine("Such a youthful creature!");
+                Console.WriteLine("\nSuch a youthful creature!");
             }
             else if (userAge < 65 && userAge >= 30)
             {
-                Console.WriteLine("With age comes wisdom!"); 
+                Console.WriteLine("\nWith age comes wisdom!");
             }
             else
             {
-                Console.WriteLine("I respect my elders!");
+                Console.WriteLine("\nI respect my elders!");
             }
-            Console.WriteLine("In what month were you born? Please type the number of the month, and then click \"Enter.\"");
+            Console.WriteLine("\nIn what month were you born? Please type the number of the month, and then click \"Enter.\"\nIf you enter a word, I will cry and go home.");
             int userMonth = int.Parse(Console.ReadLine());
             if (userMonth == 1 || userMonth == 2 || userMonth == 3 || userMonth == 11 || userMonth == 12)
             {
-                Console.WriteLine("Brrr! You were born in a cold month!");
+                Console.WriteLine("\nBrrr! You were born in a cold month!");
             }
             else if (userMonth == 4 || userMonth == 5 || userMonth == 6)
             {
-                Console.WriteLine("Ahhhh, springtime!");
+                Console.WriteLine("\nAhhhh, springtime!");
             }
             else if (userMonth == 7 || userMonth == 8)
             {
-                Console.WriteLine("Summer baby! Me too!");
+                Console.WriteLine("\nSummer baby! Me too!");
+            }
+            else 
+            {
+                Console.WriteLine("\nAutumn is such a lovely time of year.");
+            }
+            
+            Console.WriteLine("What is your favorite ROYGBIV color?\nIf you do not know what ROYGBIV is, please type \"Help\" and click \"Enter.\"");
+            string faveColor = Console.ReadLine().ToLower();
+
+
+            switch (faveColor)
+            {
+                case "help":
+                    Console.WriteLine("ROYGBIV is an acronym for the colors red, orange, yellow, green, blue, indigo, and violet.\nThink RAINBOW!");
+                    //How does the user return to the color question?
+                    break;
+                case "red":
+                    Console.WriteLine("\nRuby red!");
+                    break;
+                case "orange":
+                    Console.WriteLine("\nOrange you glad you came in today?");
+                    break;
+                case "yellow":
+                    Console.WriteLine("\nLike Yellowstone - beautiful!");
+                    break;
+                case "green":
+                    Console.WriteLine("\nGreen is my favorite color as well!");
+                    break;
+                case "blue":
+                    Console.WriteLine("\nBlue, like the skies over Keuka Lake!");
+                    break;
+                case "indigo":
+                    Console.WriteLine("\nI'm pretty sure indigo is a made up color, but OK.");
+                    break;
+                case "violet":
+                    Console.WriteLine("\nPurple - very regal!");
+                    break;
+                default:
+                    Console.WriteLine("\nYou do not know ROYGBIV? Your future looks bleak.");
+                    break;
+            }
+
+            Console.WriteLine("\nHow many siblings do you have? Enter a number, and then click \"Enter.\"");
+            int sibCount = int.Parse(Console.ReadLine());
+            
+            if(sibCount == 0)
+            {
+                Console.WriteLine("\nAn only child!");
+            }
+            else if(sibCount > 0 && sibCount <4)
+            {
+                Console.WriteLine("\nSounds like a cozy family.");
+            }
+            else if(sibCount < 0)
+            {
+                Console.WriteLine("\nBy entering a negative number, do you mean that your parents sold your siblings to the gypsies? Perhaps I know your family!");
             }
             else
             {
-                Console.WriteLine("Autumn is such a lovely time of year.");
+                Console.WriteLine("\nWow! What size wagon did your family need to get around?");
             }
-            Console.WriteLine("What is your favourite ROYGBIV colour? If you do not know what ROYGBIV is, please type \"Help\" and click \"Enter.\"");
+
+
+
+
+
+                    
+
+
+
+
+
+
             
-            string faveColor = Console.ReadLine();
-            int sibCount = int.Parse(Console.ReadLine());
-            
-            
-            
-            
-            
-            
-          
+
+
+        }
+
+    }
+}
+
+
+
+    
+
+
+
+
 
              
                
 
-        }
-    }
-}
+        
+    
+
