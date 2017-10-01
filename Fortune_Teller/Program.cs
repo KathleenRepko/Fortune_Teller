c79f2291f7cbc9f770a85fc43d079c7f1bfb5e78
KathleenRepko <kathleen.repko@gmail.com>
2017-10-01 15:19:27 -0400

    Basic project complete.

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -8,15 +8,15 @@ namespace Fortune_Teller
 {
     class Program
     {
-        private static string faveColor2;
+    
 
         static void Main(string[] args)
         {
             //Correct GitHub link is properly submitted
             //GitHub repository created from correct folder and contains solution file
-
+            //Program should be able to handle whether or not a user inputs capital or lowercase letters.
+            
             //Declaring variables and asking for user input:
-
             Console.WriteLine("Welcome to Madame Sybil's Fortune Telling Tent. I knew you were coming.");
             Console.WriteLine("\nWhat is your first name? Type it, and then click \"Enter.\"");
             string firstName = Console.ReadLine();
@@ -24,15 +24,19 @@ namespace Fortune_Teller
 
             Console.WriteLine("\nWhat is your last name? Type it, and then click \"Enter.\"");
             string lastName = Console.ReadLine();
-            Console.WriteLine("\nI new a " + lastName + " family from Poughkeepsie. Uncanny!");
+            Console.WriteLine("\nI knew a family named " + lastName + " from Poughkeepsie. Uncanny!");
 
             Console.WriteLine("\nHow many years have you been alive? Type a number, and then click \"Enter.\"");
             int userAge = int.Parse(Console.ReadLine());
-            if (userAge < 30)
+            if (userAge < 18)
             {
-                Console.WriteLine("\nSuch a youthful creature!");
+                Console.WriteLine("\nYou're just a baby!");
             }
-            else if (userAge < 65 && userAge >= 30)
+            else if (userAge >= 18 && userAge <=40)
+            {
+                Console.WriteLine("Oh, to be young again...");
+            }
+            else if (userAge > 40 && userAge < 65)
             {
                 Console.WriteLine("\nWith age comes wisdom!");
             }
@@ -40,37 +44,41 @@ namespace Fortune_Teller
             {
                 Console.WriteLine("\nI respect my elders!");
             }
-            Console.WriteLine("\nIn what month were you born? Please type the number of the month, and then click \"Enter.\"\nIf you enter a word, I will cry and go home.");
+            Console.WriteLine("\nIn what month were you born? Please type the number of the month, and then click \"Enter.\"\nIf you enter a word, I may cry and close up shop for the day.");
             int userMonth = int.Parse(Console.ReadLine());
-            if (userMonth == 1 || userMonth == 2 || userMonth == 3 || userMonth == 11 || userMonth == 12)
+            if (userMonth == 1 || userMonth == 2 || userMonth == 12)
             {
                 Console.WriteLine("\nBrrr! You were born in a cold month!");
             }
-            else if (userMonth == 4 || userMonth == 5 || userMonth == 6)
+            else if (userMonth == 3 || userMonth == 4 || userMonth == 5)
             {
                 Console.WriteLine("\nAhhhh, springtime!");
             }
-            else if (userMonth == 7 || userMonth == 8)
+            else if (userMonth == 6 || userMonth == 7 || userMonth == 8)
             {
                 Console.WriteLine("\nSummer baby! Me too!");
             }
-            else 
+            else if (userMonth == 9 || userMonth == 10 || userMonth == 11)
             {
                 Console.WriteLine("\nAutumn is such a lovely time of year.");
             }
-            
-            Console.WriteLine("What is your favorite ROYGBIV color?\nIf you do not know what ROYGBIV is, please type \"Help\" and click \"Enter.\"");
-            string faveColor = Console.ReadLine().ToLower();
+            else 
+            {
+                Console.WriteLine("\nHow are the months numbered where you live? That month doesn't exist where I come from. Moving right along...");
+            }
 
+         
+            Console.WriteLine("\nWhat is your favorite ROYGBIV color?\nIf you do not know what ROYGBIV is, please type \"Help\" and click \"Enter.\"");
+            string faveColor = Console.ReadLine().ToLower();
 
             switch (faveColor)
             {
                 case "help":
-                    Console.WriteLine("ROYGBIV is an acronym for the colors red, orange, yellow, green, blue, indigo, and violet.\nThink RAINBOW!");
-                    //How does the user return to the color question?
+                    Console.WriteLine("\nROYGBIV is an acronym for the colors red, orange, yellow, green, blue, indigo, and violet.Think RAINBOW!\nI'm sorry that you are unacquainted with ROYGBIV.");
+                    //How does the user return to the color question? Likely involves a loop.
                     break;
                 case "red":
-                    Console.WriteLine("\nRuby red!");
+                    Console.WriteLine("\nRed, like my ruby birthstone!");
                     break;
                 case "orange":
                     Console.WriteLine("\nOrange you glad you came in today?");
@@ -118,37 +126,34 @@ namespace Fortune_Teller
             //Conditionals:
 
             //If the user’s age is an odd number, then they will retire in ____ years, or ___ years if their age is an even number.
-            string retireAge;
+            int retireAge;
             if (userAge % 2 == 0)
             {
-                retireAge = "even";
+                retireAge = 10;
             }
             else
             {
-                retireAge = "odd";
+                retireAge = 7;
             }
-            //If the user’s number of siblings is 0, then they will have a vacation home in ____(location),
+
+            //Vacation home is based on number of sibling user entered.
             string vacaHome;
             if(sibCount == 0)
             {
                 vacaHome = "New York City";
             } 
-            //or 1 then they will have a vacation home in ____(location),
             else if(sibCount == 1)
             {
                 vacaHome = "Chicago";
             }
-            //or 2 then they will have a vacation home in ____(location), 
             else if(sibCount == 2)
             {
                 vacaHome = "Omaha";
             }
-            //or 3 then they will have a vacation home in ____(location), 
             else if(sibCount == 3)
             {
                 vacaHome = "Key West";
             }
-            //or more than 3 then they will have a vacation home in ____(location).
             else if(sibCount > 3)
             {
                 vacaHome = "Sedona";
@@ -159,31 +164,62 @@ namespace Fortune_Teller
                 vacaHome = "Siberia";
             }
 
-            //Depending on which ROYGBIV color is the user’s favorite, they will have a specific mode of transportation(i.e.car, boat, plane, etc.)
+            //Mode of transportation is determined by user's favorite color.
             string modeTransportation;
-            switch faveColor
-                {
-                case red:
-                    modeTransportation = "sportscar";
+            switch (faveColor)
+            {
+                case "red":
+                    modeTransportation = "little red Corvette";
                     break;
-                case orange:
-                    modeTransportation = "scooter";S
-
+                case "orange":
+                    modeTransportation = "scooter";
+                    break;
+                case "yellow":
+                    modeTransportation = "hoverboard";
+                    break;
+                case "green":
+                    modeTransportation = "Jaguar in British Racing Green";
+                    break;
+                case "blue":
+                    modeTransportation = "hot air balloon";
+                    break;
+                case "indigo":
+                    modeTransportation = "VW bus";
+                    break;
+                case "violet":
+                    modeTransportation = "Prius";
+                    break;
+                default:
+                    modeTransportation = "Ford Pinto";
+                    break;
             }
-                
 
-            //If the user’s birth month is 1 - 4, they will have $____ in the bank; 
-            //if the user's birth month is 5-8, they will have $____ in the bank; 
-            //and if it is 9-12, then they will have $____ in the bank. 
-            //If the user enters something other than 1-12 as their birth month, they will have $0.00 in the bank.
+            //User's retirement funds are determined by user's birth month.
+            int cashBank;
+            if (userMonth >= 1 && userMonth <= 4)
+            {
+                cashBank = 250000;
+            }
+            else if (userMonth >=5 && userMonth <= 8)
+            {
+                cashBank = 500000;
+            }
+            else if (userMonth >= 9 && userMonth <= 12)
+            {
+                cashBank = 750000;
+            }
+            else
+            {
+                cashBank = 0;
+            }
 
             //The user’s fortune should be written as such:
             //[First Name] [Last Name] will retire in [# of Years] with [Amount of Money] in the bank, a vacation home in [Location] and a [Mode of Transportation].
-            //Program should be able to handle whether or not a user inputs capital or lowercase letters.
+            Console.WriteLine("\nI will now look into my crystal ball and see what your future holds...\n     ...\n          ...\n               ...");
+            Console.WriteLine(firstName + " " + lastName + " will retire in " + retireAge + " years, with $" + cashBank + " in the bank, a vacation home in " + vacaHome + ", and a " + modeTransportation + " with which to get around.");
+            Console.WriteLine("\nMadame Sybil has spoken. Have a wonderful life!");
 
 
-            Console.WriteLine("\nI look in the crystal ball, and Spirit is telling me this:");
-            Console.WriteLine(firstName + " " + lastName " will retire in " + )
 
 
 
